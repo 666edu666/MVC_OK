f3ccf0fdda2ddcb46137d32e10264114b8ea219d
Eduardo Bermúdez Blanco <Eduardo.Bermudez@tajamar365.com>
2019-01-17 10:44:35 +0100

    dasd

---
+++ InyeccionBBDD/Controllers/DepartamentosController.cs
@@ -0,0 +1,24 @@
+﻿using RepositorioHospital.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InyeccionBBDD.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        IRepositoryDepartamentos repo;
+        // GET: Departamentos
+
+        public DepartamentosController(IRepositoryDepartamentos repo)
+        {
+            this.repo = repo;
+        }
+        public ActionResult Index()
+        {
+            return View(this.repo.GetDepartamentos());
+        }
+    }
+}
